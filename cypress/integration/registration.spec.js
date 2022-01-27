dc0334a55d69a74123f5599d41656bf4f7525578
Gustavo Ramos <gustavo.rdev@gmail.com>
2022-01-26 23:44:28 -0300

    Upgrades on registration test and adding cypress file-upload lib to the project

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -9,5 +9,53 @@ describe("Registration", () => {
       "Cadastre-se para  fazer entregas"
     );
     cy.url().should("be.equal", `https://buger-eats.vercel.app/deliver`);
+
+    var deliveryman = {
+      name: "Gustavo Ramos",
+      cpf: "09909911109",
+      email: "gustavo@hotmail.com",
+      whatsapp: "11999999999",
+      address: {
+        postalcode: "04534011",
+        street: "Rua Joaquim Floriano",
+        addressNumber: "1000",
+        complement: "Ap 142",
+        district: "Itaim Bibi",
+        city: "São Paulo/SP",
+      },
+      deliveryMethod: "Moto",
+      cnh: "images/cnh-digital.jpg",
+    };
+
+    cy.get('input[name="name"]').type(deliveryman.name);
+    cy.get('input[name="cpf"]').type(deliveryman.cpf);
+    cy.get('input[name="email"]').type(deliveryman.email);
+    cy.get('input[name="whatsapp"]').type(deliveryman.whatsapp);
+
+    cy.get('input[name="postalcode"]').type(deliveryman.address.postalcode);
+    cy.get('input[type="button"][value="Buscar CEP"]').click();
+    cy.get('input[name="address-number"]').type(
+      deliveryman.address.addressNumber
+    );
+    cy.get('input[name="address-details"]').type(
+      deliveryman.address.complement
+    );
+
+    cy.get('input[name="address"]').should(
+      "have.value",
+      deliveryman.address.street
+    );
+    cy.get('input[name="district"]').should(
+      "have.value",
+      deliveryman.address.district
+    );
+    cy.get('input[name="city-uf"]').should(
+      "have.value",
+      deliveryman.address.city
+    );
+
+    cy.contains(".delivery-method li", deliveryman.deliveryMethod).click();
+
+    cy.get('input[accept^="image"]').attachFile(deliveryman.cnh);
   });
 });
